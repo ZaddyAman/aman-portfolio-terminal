ad42f5f2186f07272fc7377640349317cf7dbfd6
Aman Sayyad <91181377+ZaddyAman@users.noreply.github.com>
2025-07-19 21:47:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/aman-portfolio-terminal/
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
